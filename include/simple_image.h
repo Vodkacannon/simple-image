ced6bf1d9366b44fcd9b1c8faf06eb08f512153d
Armend Veseli <armend.veseli@yahoo.com>
2021-10-11 12:03:26 -0400

    Create simple_image.h

---
+++ include/simple_image.h
@@ -0,0 +1,26 @@
+struct pixel_rgb
+{
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
+struct pixel_rgba
+{
+  unsigned char r;
+	unsigned char g;
+	unsigned char b;
+	unsigned char a;
+};
+
+struct image_rgb
+{
+  struct pixel_rgb* buffer_x;
+	struct pixel_rgb* buffer_y;
+};
+
+struct image_rgba
+{
+	struct pixel_rgba* buffer_x;
+	struct pixel_rgba* buffer_y;
+};
