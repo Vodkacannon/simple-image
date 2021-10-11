78f4f7d0e8601b02b3b8478b797756723c0877af
Armend Veseli <armend.veseli@yahoo.com>
2021-10-11 13:17:26 -0400

    Update simple_image.c

--- src/simple_image.c
+++ src/simple_image.c
@@ -1,6 +1,21 @@
 #include "simple_image.h"
 
-void delete_image_rgba(struct image_rgba* my_image)
+void delete_image_rgba(struct image_rgb* my_image)
 {
 	free(my_image);
+}
+
+void image_rgb_get_buffer_x_length(struct image_rgb* my_image)
+{
+	return sizeof(my_image->buffer_x / my_image)
+}
+
+void image_rgb_set_to_color(struct image_rgb* my_image)
+{
+	int size = image_rgb_get_buffer_x_length(&my_image);
+	
+	for(int x = image_rgb_get_buffer_x_length; x < size; size++)
+	{
+		//pussy.
+	}
 }
