8815bc1306634227c7c42309382a883bf56616ab
Armend Veseli <armend.veseli@yahoo.com>
2021-10-11 14:13:22 -0400

    Update simple_image.c

--- src/simple_image.c
+++ src/simple_image.c
@@ -17,11 +17,12 @@ void image_rgb_get_buffer_y_size(struct image_rgb* my_image)
 
 void image_rgb_set_to_color(struct image_rgb* my_image, struct pixel_rgb* my_pixel)
 {
-	int size = image_rgb_get_buffer_x_size(&my_image);
+	int size_x = image_rgb_get_buffer_x_size(&my_image);
+	int size_y = image_rgb_get_buffer_x_size(&my_image);
 	
-	for(int x = image_rgb_get_buffer_x_size; x > 0; --x)
+	for(int x = size_x - 1; x > 0; --x)
 	{
-		for(int y = image_rgb_get_buffer_y_size; y > 0; --y)
+		for(int y = size_y - 1; y > 0; --y)
 		{
 			my_image->buffer_x[x] = {my_pixel->r, my_pixel->g, my_pixel->b};
 			my_image->buffer_y[y] = {my_pixel->r, my_pixel->g, my_pixel->b};
