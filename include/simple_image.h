f8f502912df3485225f9eed92fbd6cc6e4ec5daa
Armend Veseli <armend.veseli@yahoo.com>
2021-10-13 01:45:38 -0400

    Update simple_image.h

--- include/simple_image.h
+++ include/simple_image.h
@@ -1,26 +1,4 @@
-struct pixel_rgb
-{
-	unsigned char r;
-	unsigned char g;
-	unsigned char b;
-};
+#pragma once
 
-struct pixel_rgba
-{
-	unsigned char r;
-	unsigned char g;
-	unsigned char b;
-	unsigned char a;
-};
-
-struct image_rgb
-{
-	struct pixel_rgb* buffer_x;
-	struct pixel_rgb* buffer_y;
-};
-
-struct image_rgba
-{
-	struct pixel_rgba* buffer_x;
-	struct pixel_rgba* buffer_y;
-};
+#include "simple_image_rgb.h"
+#include "simple_image_rgba.h"
