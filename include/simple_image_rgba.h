96c1bfb2213398132064a893c6adcbc90de3befd
Armend Veseli <armend.veseli@yahoo.com>
2021-10-13 01:50:34 -0400

    Update simple_image_rgba.h

--- include/simple_image_rgba.h
+++ include/simple_image_rgba.h
@@ -13,3 +13,13 @@ struct image_rgba
 	struct pixel_rgba* buffer_x;
 	struct pixel_rgba* buffer_y;
 };
+
+void image_rgba_delete(struct image_rgba* my_image);
+
+void image_rgba_delete(struct image_rgba* my_image);
+
+void image_rgba_get_buffer_x_size(struct image_rgba* my_image);
+
+void image_rgba_get_buffer_y_size(struct image_rgba* my_image);
+
+void image_rgba_set_to_color(struct image_rgba* my_image, struct pixel_rgba* my_pixel);
