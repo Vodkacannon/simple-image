7367f5bc433ac647714256704799d901b2108c30
Armend Veseli <armend.veseli@yahoo.com>
2021-10-13 01:42:55 -0400

    Create simple_image_rgb.h

---
+++ include/simple_image_rgb.h
@@ -0,0 +1,11 @@
+#pragma once
+
+void image_rgb_delete(struct image_rgb* my_image);
+
+void image_rgb_delete(struct image_rgb* my_image);
+
+void image_rgb_get_buffer_x_size(struct image_rgb* my_image);
+
+void image_rgb_get_buffer_y_size(struct image_rgb* my_image);
+
+void image_rgb_set_to_color(struct image_rgb* my_image, struct pixel_rgb* my_pixel);
