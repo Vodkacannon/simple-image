dc3b58a03637a6f2cf73dacccc02abc2f04ff9ca
Armend Veseli <armend.veseli@yahoo.com>
2021-10-13 04:03:44 -0400

    Update simple_image_rgb.h

--- include/simple_image_rgb.h
+++ include/simple_image_rgb.h
@@ -27,4 +27,6 @@ void image_rgb_get_buffer_x_size(struct image_rgb* my_image);
 
 void image_rgb_get_buffer_y_size(struct image_rgb* my_image);
 
+void image_rgb_set_pixel(struct image_rgb* my_image, struct pixel_rgb* my_pixel, int pixel_position_y, int pixel_position_z);
+
 void image_rgb_set_to_color(struct image_rgb* my_image, struct pixel_rgb* my_pixel);
