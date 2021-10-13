0fa93e804a74277482c431a567a721ec38b4076b
Armend Veseli <armend.veseli@yahoo.com>
2021-10-13 01:47:09 -0400

    Update simple_image_rgb.c

--- src/simple_image_rgb.c
+++ src/simple_image_rgb.c
@@ -1,4 +1,4 @@
-#include "simple_image.h"
+#include "simple_image_rgb.h"
 
 void image_rgb_delete(struct image_rgb* my_image)
 {
@@ -12,18 +12,18 @@ void image_rgb_delete(struct image_rgb* my_image)
 
 void image_rgb_get_buffer_x_size(struct image_rgb* my_image)
 {
-	return sizeof(my_image->buffer_x / my_image)
+	return sizeof(my_image->buffer_x) / sizeof(my_image->buffer_x[0]);
 }
 
 void image_rgb_get_buffer_y_size(struct image_rgb* my_image)
 {
-	return sizeof(my_image->buffer_x / my_image)
+	return sizeof(my_image->buffer_y) / sizeof(my_image->buffer_y[0]);
 }
 
 void image_rgb_set_to_color(struct image_rgb* my_image, struct pixel_rgb* my_pixel)
 {
 	int size_x = image_rgb_get_buffer_x_size(&my_image);
-	int size_y = image_rgb_get_buffer_x_size(&my_image);
+	int size_y = image_rgb_get_buffer_y_size(&my_image);
 	
 	for(int x = size_x - 1; x > 0; --x)
 	{
